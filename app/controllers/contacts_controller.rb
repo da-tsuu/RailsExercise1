3e4f999a2c8f361e5d97366771e5817ae0a0b61a
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-10-01 09:19:14 +0000

    rails入門演習課題1を再々提出

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,20 @@
 class ContactsController < ApplicationController
-
   def new
     @contact = Contact.new
   end
 
   def create
     @contact = Contact.new(contact_params)
-
-    if @contact.save(contact_params)
-      redirect_to new_contacts_path
+    if @contact.save
+      redirect_to new_contact_path
     else
-      render 'new'
+      render :new
     end
   end
 
   private
 
   def contact_params
-   params.require(:contact).permit(:name, :email,:content )
+    params.require(:contact).permit(:name, :email, :content)
   end
-
 end
